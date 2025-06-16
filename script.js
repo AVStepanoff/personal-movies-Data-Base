26e4fe5660da8d4312d4f89e0f31b32fa0db59a4
Aleksey <psstudio.info@gmail.com>
2025-06-17 00:17:09 +0300

    Используем циклы

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -10,13 +10,26 @@ const personalMovieDB = {
   privat: false,
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", "");
-const b = prompt("На сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
+  let a = prompt("Один из последних просмотренных фильмов?", "");
+  let b = +prompt("На сколько оцените его?", "");
 
-const a1 = prompt("Один из последних просмотренных фильмов?", "");
-const b1 = prompt("На сколько оцените его?", "");
+  if (a != "" && a != null && b != "" && b != null && a.length <= 50) {
+    personalMovieDB.movies[a] = b;
+  } else {
+    i--;
+  }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[a1] = b1;
+
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
